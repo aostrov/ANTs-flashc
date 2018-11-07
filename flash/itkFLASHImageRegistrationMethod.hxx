9ef9dc6b5b738b947ad83a43a22a1d7d9c55e464
gfleishman <greg.nli10me@gmail.com>
2018-11-06 20:21:11 -0500

    debugs for John's test

--- flash/itkFLASHImageRegistrationMethod.hxx
+++ flash/itkFLASHImageRegistrationMethod.hxx
@@ -144,7 +144,7 @@ FLASHImageRegistrationMethod<TFixedImage, TMovingImage, TOutputTransform, TVirtu
                             Vec3Di(origin[0], origin[1], origin[2]));
     Field3D * v0SpatialNew = new Field3D(this->m_grid, this->m_mType);
     // debug
-    ITKFileIO::SaveField(*v0Spatial, "v0_spatial_before_resample.mhd");
+    ITKFileIO::SaveImage(*v0Spatial, "v0_spatial_before_resample.mhd");
     // end: debug
     Opers::Resample(*v0SpatialNew, *v0Spatial);
     // debug
